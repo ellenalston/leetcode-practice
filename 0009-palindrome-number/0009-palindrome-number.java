187394fa8f6b08db28f1479c0098253524e0f49a
Ellen Alston <ellenantes@gmail.com>
2024-11-20 10:59:12 -0500

    Time: 4 ms (100%), Space: 43.8 MB (65.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int reverse = 0;
+        int temp = x;
+
+        if (temp < 0) {
+            return false;
+        }
+
+        while (temp != 0) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = temp / 10;
+        }
+
+        return (reverse == x);
+
+    }
+}
