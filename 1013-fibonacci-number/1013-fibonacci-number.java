e4180ef547195c723271d0e7d8190bc687df3bf0
Ellen Alston <ellenantes@gmail.com>
2024-11-21 14:20:52 -0500

    Time: 0 ms (100%), Space: 40.6 MB (20.16%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1){
+        if (n <= 1) {
             return n;
         }
 
-       return fib(n-1) + fib(n-2); 
+        int[] fibArray = new int[n + 1];
+
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+
+        return fibArray[n];
     }
 }
